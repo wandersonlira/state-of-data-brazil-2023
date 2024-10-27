51f8974ae68b504b5c5690a27f10b86de539be76
Wanderson Lira <wandersonlira.dev1@gmail.com>
2024-10-26 23:39:16 -0300

    :wastebasket: remove: Removendo arquivos não utilizados do projeto para manter a organização e atualização contínua

--- src/utils/.ipynb_checkpoints/utils-checkpoint.py
+++
@@ -1,3 +0,0 @@
-def calcular_valor_coluna(merged):
-  merged['total_percentual'] = (merged['total'] / merged['total_nivel']) * 100
-  return merged
---
+++ src/utils/funcoes.py
@@ -0,0 +1,9 @@
+def calcular_valor_coluna(merged):
+  # merged['valores_percentuais'] = (merged['valores_totais'] / merged['total_experiencia_profissional_afetada']) * 100
+  return merged
+
+
+
+def print_ola(nome):
+    a = nome
+    return a
